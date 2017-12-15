1f49a5a1162686eb353f74315d286d20c577a232
Emily Geller <emilygeller@Emilys-MacBook-Pro.local>
2017-12-14 16:22:24 -0800

    added datetime solutions

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,32 @@
 # Hint:  use Google to find python function
 
+import datetime
+
 ####a) 
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
+
+d1 = datetime.datetime.strptime(date_start, '%m-%d-%Y')
+d2 = datetime.datetime.strptime(date_stop, '%m-%d-%Y')
+
+print(abs((d2-d1).days))
 
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+d3 = datetime.datetime.strptime(date_start, '%m%d%Y')
+d4 = datetime.datetime.strptime(date_stop, '%m%d%Y')
+
+print(abs((d4-d3).days))
+
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+d5 = datetime.datetime.strptime(date_start, '%d-%b-%Y')
+d6 = datetime.datetime.strptime(date_stop, '%d-%b-%Y')
+
+print(abs((d6-d5).days))
+
+
